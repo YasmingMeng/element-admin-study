3787d337d24f7fca6a0f67403f394b0c60cea220
mengyixue <1661237617@qq.com>
2021-11-23 18:33:33 +0800

    框架搭建(vita, vue-cli)

---
+++ element-study（Vue-3)/element-study/App.vue
@@ -0,0 +1,16 @@
+<!--
+ * @Description: 
+ * @Date: 2021-11-09 16:04:31
+ * @LastEditTime: 2021-11-09 16:04:43
+-->
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
